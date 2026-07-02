015b00918308673f7a783f1ddc20f045f7a8e5ef
syedzaiba <syedzaibanigar@gmail.com>
2026-07-02 18:15:49 +0530

    Remove terser dependency

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite';
+
 export default defineConfig({
     root: './',
     publicDir: 'public',
-    build: { outDir: 'dist', minify: 'terser' },
-    server: { port: 3000, open: true }
+    build: {
+        outDir: 'dist',
+        assetsDir: 'assets',
+        emptyOutDir: true
+    },
+    server: {
+        port: 3000,
+        open: true
+    }
 });
